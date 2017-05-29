9b885766ed0bb3508f68398008659378526b2146
Oleksandr Lukhanin <xander_uzzzer@mail.ru>
2017-05-29 09:35:45 +0300

    unnecessary commit

---
+++ FileManager/Browser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileManager
+{
+    class Browser
+    {
+        DirectoryInfo currentDir;
+        FileInfo[] files;
+        DirectoryInfo[] dirs;
+
+        public Browser()
+        {
+            currentDir = new DirectoryInfo(@"C:\");
+            GetCurrentItems();
+        }
+
+        public void GetCurrentItems()
+        {
+            dirs = currentDir.GetDirectories();
+            files = currentDir.GetFiles();
+            ShowContent();
+        }
+
+        public void ShowContent()
+        {
+            //Console.Clear();
+            foreach (var dir in dirs)
+            {
+                Console.WriteLine(dir.Name + "\t\t<dir>");
+            }
+                
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.Name + $"\t\t<{file.Extension}>");
+            }
+                
+            Console.WriteLine();
+        }
+
+        public void OpenFolder(string dirName)
+        {            
+            currentDir = new DirectoryInfo (currentDir.Name + dirName);
+            GetCurrentItems();
+        }
+        public void CloseFolder()
+        {
+            currentDir = currentDir.Parent;
+            GetCurrentItems();
+        }
+
+        public void SetActiveItem()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+    }
+}
--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FileManager
@@ -11,8 +13,68 @@ namespace FileManager
         static void Main(string[] args)
         {
             Console.WriteLine("File manager");
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.Clear();
+
+            Console.SetWindowSize(40, 31);
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+
+            DrawBorder(10, 10, 20, 30);
 
             Console.ReadLine();
+        }
+        private static void WriteCharacterStrings(int start, int end,
+                                             bool changeColor)
+        {
+            for (int ctr = start; ctr <= end; ctr++)
+            {
+                if (changeColor)
+                    Console.BackgroundColor = (ConsoleColor)((ctr - 1) % 16);
+
+                Console.WriteLine(new String(Convert.ToChar(ctr + 64), 30));
+            }
+        }
+        static void DrawBorder(int x, int y, int w, int h)
+        {
+            // Draw top line
+            Console.SetCursorPosition(x, y);
+            Console.Write("╔");
+            for (int i = x + 1; i < w - 1; i++)
+            {
+                Console.Write("═");
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("╗");
+
+            // Draw Bottom line
+            Console.SetCursorPosition(x, h);
+            Console.Write("╚");
+            for (int i = x + 1; i < w - 1; i++)
+            {
+                Console.Write("═");
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("╝");
+
+            // Draw left line
+            for (int i = y + 1; i < h; i++)
+            {
+                Console.SetCursorPosition(x, i);
+                Console.WriteLine("║");
+                Thread.Sleep(50);
+            }
+
+            // Draw left line
+            for (int i = y + 1; i < h; i++)
+            {
+                Console.SetCursorPosition(w - 1, i);
+                Console.WriteLine("║");
+                Thread.Sleep(50);
+            }
+
+            Console.SetCursorPosition(x + 1, y + 1);
+            Console.WriteLine("test");
+            Console.WriteLine("test");
         }
     }
 }
